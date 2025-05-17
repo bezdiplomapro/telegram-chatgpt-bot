69a31263d7c1f37fc7c61fab44cc4efc94f1e06a
bezdiplomapro <75723201+bezdiplomapro@users.noreply.github.com>
2025-05-17 18:56:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-openai.api_key = "sk-ВСТАВЬ_СЮДА_КЛЮЧ"
 
-TELEGRAM_TOKEN = "ВСТАВЬ_ТЕЛЕГРАМ_ТОКЕН"
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+TELEGRAM_TOKEN = "7635370036:AAEeZC2HReNw7P2H9xNeaI3F0qZr1uB161o"
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
 @app.route("/", methods=["POST"])
